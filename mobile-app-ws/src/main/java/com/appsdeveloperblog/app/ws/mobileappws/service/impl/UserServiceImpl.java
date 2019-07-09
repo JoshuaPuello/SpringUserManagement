65cf8b2f2f2b2d3f2b198ff534023b772750d7d7
Joshua Puello <joshuapuello@outlook.com>
2019-07-09 10:59:53 -0500

    [JPP] - UserService interface and impl

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.app.ws.mobileappws.service.impl;
+
+import com.appsdeveloperblog.app.ws.mobileappws.service.UserService;
+import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDTO createUser(UserDTO user) {
+        return null;
+    }
+}
