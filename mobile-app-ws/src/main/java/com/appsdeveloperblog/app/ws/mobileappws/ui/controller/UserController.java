22c70346788c4fa73a89c3f31f1b01622c914254
Joshua Puello <joshuapuello@outlook>
2019-07-09 10:55:43 -0500

    [JPP] - User creation service

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDTO;
 import com.appsdeveloperblog.app.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.mobileappws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +23,16 @@ public class UserController {
 
     @PostMapping
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
-        return null;
+
+        UserRest returnUser = new UserRest();
+
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(userDetails, userDTO);
+
+        UserDTO createdUser = userService.createUser(userDTO);
+        BeanUtils.copyProperties(createdUser, returnUser);
+
+        return returnUser;
     }
 
     @PutMapping
