11a0b865193cab11501bfaff6eb642af175b6471
Joshua Puello <joshuapuello@outlook>
2019-07-09 10:59:53 -0500

    [JPP] - UserService interface and impl

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/service/UserService.java
@@ -0,0 +1,8 @@
+package com.appsdeveloperblog.app.ws.mobileappws.service;
+
+import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDTO;
+
+public interface UserService {
+
+    UserDTO createUser(UserDTO user);
+}
