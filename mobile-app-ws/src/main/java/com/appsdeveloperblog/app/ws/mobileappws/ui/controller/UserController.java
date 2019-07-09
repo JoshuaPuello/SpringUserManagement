bbf18ccb820764da3a62be051292aa22061c621f
Joshua Puello <joshuapuello@outlook>
2019-07-09 10:40:52 -0500

    [JPP] - UserDetailsRequestModel class

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.mobileappws.ui.model.request.UserDetailsRequestModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser() {
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
         return "create user was called";
     }
 
