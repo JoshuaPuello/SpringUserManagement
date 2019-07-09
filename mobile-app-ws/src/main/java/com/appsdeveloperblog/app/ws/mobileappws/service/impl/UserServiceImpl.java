66e5765a3a5a208cff747d1ded2e976b822e9af2
Joshua Puello <joshuapuello@outlook>
2019-07-09 11:30:41 -0500

    [JPP] - User creation service layer

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/service/impl/UserServiceImpl.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/service/impl/UserServiceImpl.java
@@ -1,14 +1,33 @@
 package com.appsdeveloperblog.app.ws.mobileappws.service.impl;
 
+import com.appsdeveloperblog.app.ws.mobileappws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.mobileappws.repository.UserRepository;
 import com.appsdeveloperblog.app.ws.mobileappws.service.UserService;
 import com.appsdeveloperblog.app.ws.mobileappws.shared.dto.UserDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDTO createUser(UserDTO user) {
-        return null;
+    public UserDTO createUser(UserDTO userDTO) {
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDTO, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUser = userRepository.save(userEntity);
+
+        UserDTO returnUser = new UserDTO();
+        BeanUtils.copyProperties(storedUser, returnUser);
+
+        return returnUser;
     }
 }
