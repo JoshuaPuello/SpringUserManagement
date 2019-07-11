5e03edc4ba33c65b50ff95f898bcd22b658a6715
Joshua Puello <joshuapuello@outlook.com>
2019-07-11 12:55:22 -0500

    [JPP] - Changes to generate WAR file

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/MobileAppWsApplication.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/MobileAppWsApplication.java
@@ -3,11 +3,18 @@ package com.appsdeveloperblog.app.ws.mobileappws;
 import com.appsdeveloperblog.app.ws.mobileappws.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
