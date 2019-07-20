e827abccfe6703d7e2b6d8834129b65115b6e7c8
Joshua Puello <joshuapuello@outlook.com>
2019-07-19 23:45:45 -0500

    [JPP] - HATEOAS support

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/model/response/AddressRest.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/mobileappws/ui/model/response/AddressRest.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.app.ws.mobileappws.ui.model.response;
 
-public class AddressRest {
+import org.springframework.hateoas.ResourceSupport;
+
+public class AddressRest extends ResourceSupport {
 
     private String addressId;
     private String city;
